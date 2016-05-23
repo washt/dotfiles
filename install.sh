7f8cb77067e7465905d1e275af80776f3cc2ffe1
Tucker Wash <washt23@gmail.com>
2016-05-22 22:40:25 -0400

    added .inputrc to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+cp .inputrc ~./inputrc
 cp .bashrc ~/.bashrc;
 cp .bash_aliases ~/.bash_aliases;
 cp .vimrc ~/.vimrc;
