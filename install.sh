7f44b2cf80cc1af4265aa4b75861c58742107138
Tucker Wash <washt23@gmail.com>
2016-12-13 21:01:47 -0500

    fixed typos :clap:

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
-cp -f .inputrc ~./inputrc
+cp -f .inputrc ~/.inputrc;
 cp -f .bashrc ~/.bashrc;
 cp -f .bash_aliases ~/.bash_aliases;
 cp -f .vimrc ~/.vimrc;
 mkdir -f ~/.vim;
-cp -f .vim ~/vim;
+cp -rf .vim ~/vim;
