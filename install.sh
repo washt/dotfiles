21410001d57c5fb73e0dcb174841455725c65341
Tucker Wash <tucker@hopin.to>
2020-12-30 12:40:07 -0500

    updates install script

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 cp -f .inputrc ~/.inputrc;
 cp -f .bashrc ~/.bashrc;
 cp -f .bash_aliases ~/.bash_aliases;
 cp -f .vimrc ~/.vimrc;
-mkdir -f ~/.vim;
-cp -rf .vim ~/vim;
